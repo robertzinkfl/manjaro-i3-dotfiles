593a061befca63f609fcd01346ea921b31574b4a
Robert Zink <robertzinkfl@gmail.com>
2022-07-05 02:10:01 -0400

    Fixed some oopsies

--- install_pacman.sh
+++ install_pacman.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo pacman -S alacritty exa code i3-gaps autorandr firefox pcmanfm polybar
+sudo pacman -S alacritty exa code i3-gaps autorandr firefox pcmanfm polybar ttf-font-awesome
--- load_config.sh
+++ load_config.sh
@@ -2,16 +2,21 @@
 pwd=$(pwd)
 echo "Working directory is: $pwd"
 echo ''
+echo "Making directories..."
+rm -r $HOME/.i3
+rm -r $HOME/.config/polybar
+mkdir $HOME/.i3
+mkdir $HOME/.config/polybar
 echo "Making symbolic links..."
 echo "Loading i3 config..."
-ln -sf $pwd/i3/config $HOME/.i3
-ln -sf $pwd/i3/one_monitor.sh $HOME/.i3
-ln -sf $pwd/i3/two_monitors.sh $HOME/.i3
+ln -sf $pwd/i3/config $HOME/.i3/
+ln -sf $pwd/i3/one_monitor.sh $HOME/.i3/
+ln -sf $pwd/i3/two_monitors.sh $HOME/.i3/
 
 echo "Loading polybar config..."
-ln -sf $pwd/polybar/config.ini $HOME/.config/polybar
-ln -sf $pwd/polybar/launch.sh $HOME/.config/polybar
-ln -sf $pwd/polybar/updates-pacman.sh $HOME/.config/polybar
+ln -sf $pwd/polybar/config.ini $HOME/.config/polybar/
+ln -sf $pwd/polybar/launch.sh $HOME/.config/polybar/
+ln -sf $pwd/polybar/updates-pacman.sh $HOME/.config/polybar/
 
 echo "Loading autorandr config..."
 rm -r $HOME/.config/autorandr
